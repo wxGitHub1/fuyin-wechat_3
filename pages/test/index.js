14d1d1e6d8ea6b5161f54b8d2724c6edbce99df3
wangxu <1165328009@qq.com>
2020-06-27 22:50:13 +0800

    修改预约需求

--- pages/test/index.js
+++ pages/test/index.js
@@ -15,19 +15,54 @@ Page({
     imgUrls: '/images/img_shjiantou_fds.png',
     clientHeight: '',
     guide: app.globalData.guide,
+    titleData: {
+      "bg_color": "#A1BA50",
+      "color": "#fff",
+      "flag": 0,
+      "name": "预约到访"
+    },
+    hasEmptyGrid: false,
+    cur_year: '',
+    cur_month: '',
+    to_month: '',
+    hiddenmodalput: true,
+    yuyue_phone: null,
+    yuyue_date: null,
+    longitude: 108.9483207500,
+    latitude: 34.3491318800,
+    covers: [{
+      latitude: 34.3491318800,
+      longitude: 108.9483207500,
+      iconPath: '/images/location.png'
+    }]
   },
-  Appointment_customization_func:function(){
+  //导航
+  daohang: function () {
     var that = this
-    wx.navigateTo({
-      url:'/pages/reservation/index',
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        wx.openLocation({ //​使用微信内置地图查看位置。
+          latitude: that.data.latitude, //要去的纬度-地址
+          longitude: that.data.longitude, //要去的经度-地址
+          name: "经开万科中心",
+          address: '未央区未央路301号'
+        })
+      }
     })
   },
-  about_us_func:function(){
-    var that = this
-    that.setData({
-      swiperCurrent: 5
-    })
-  },
+  // Appointment_customization_func:function(){
+  //   var that = this
+  //   wx.navigateTo({
+  //     url:'/pages/reservation/index',
+  //   })
+  // },
+  // about_us_func:function(){
+  //   var that = this
+  //   that.setData({
+  //     swiperCurrent: 5
+  //   })
+  // },
   // 登录+授权获取手机号
   getPhoneNumber(e) {
     var that = this
@@ -154,7 +189,8 @@ Page({
           }
         }
       }),
-      console.log("app传输过来" + this.data.guide)
+      // console.log("app传输过来" + this.data.guide)
+      this.setNowDate();
   },
   /**
    * 用户点击右上角分享
@@ -165,6 +201,150 @@ Page({
       title: '让每个孩子更健康的发育',
       path: '/pages/test/index?id='+that.data.guide,
       imageUrl: '/images/IMG_20200616_155844.jpg' //这个是分享的图片
+    }
+  },
+  dateSelectAction: function (e) {
+    var that = this
+    // this.setData({
+    //   hiddenmodalput: !this.data.hiddenmodalput
+    // })
+
+    var cur_day = e.currentTarget.dataset.idx;
+    console.log(e)
+    this.setData({
+      // todayIndex: cur_day,
+      yuyue_date: `${this.data.cur_year}-${this.data.cur_month}-${cur_day + 1}`,
+    })
+    console.log(`点击的日期:${this.data.cur_year}年${this.data.cur_month}月${cur_day + 1}日`);
+
+    // console.log(this.data.todayIndex)
+    console.log(this.data.yuyue_date)
+    
+    console.log(app.globalData.myPhone)
+    console.log(that.data.yuyue_phone)
+    console.log(that.data.yuyue_date)
+    wx.request({
+      url: 'http://192.168.1.108:80/wx/wxReserve',
+      method: "POST",
+      data: {
+        phone: app.globalData.myPhone,
+        reservePhone:app.globalData.myPhone,
+        reserveTime:that.data.yuyue_date,
+      },
+      header: {
+        "Content-Type": "application/json"
+      },
+      success: function (res) {
+        console.log(res)
+        if (res.data.returnCode != 0) {
+          wx.showToast({
+            title: "预约失败！",
+            icon: 'warning',
+            duration: 2000
+          })
+        } else {
+          console.log(res)
+          wx.showToast({
+            title: '预约成功！',
+            icon: 'success',
+            duration: 2000
+          })
+        }
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
+  },
+
+  setNowDate: function () {
+    const date = new Date();
+    const cur_year = date.getFullYear();
+    const cur_month = date.getMonth() + 1;
+    const todayIndex = date.getDate() - 1;
+    console.log(`日期：${todayIndex}`)
+    const weeks_ch = ['日', '一', '二', '三', '四', '五', '六'];
+    this.calculateEmptyGrids(cur_year, cur_month);
+    this.calculateDays(cur_year, cur_month);
+    this.setData({
+      cur_year: cur_year,
+      cur_month: cur_month,
+      to_month: cur_month,
+      weeks_ch,
+      todayIndex,
+    })
+    console.log(this.data)
+  },
+
+  getThisMonthDays(year, month) {
+    return new Date(year, month, 0).getDate();
+  },
+  getFirstDayOfWeek(year, month) {
+    return new Date(Date.UTC(year, month - 1, 1)).getDay();
+  },
+  calculateEmptyGrids(year, month) {
+    const firstDayOfWeek = this.getFirstDayOfWeek(year, month);
+    let empytGrids = [];
+    if (firstDayOfWeek > 0) {
+      for (let i = 0; i < firstDayOfWeek; i++) {
+        empytGrids.push(i);
+      }
+      this.setData({
+        hasEmptyGrid: true,
+        empytGrids
+      });
+    } else {
+      this.setData({
+        hasEmptyGrid: false,
+        empytGrids: []
+      });
+    }
+  },
+  calculateDays(year, month) {
+    let days = [];
+    const thisMonthDays = this.getThisMonthDays(year, month);
+    for (let i = 1; i <= thisMonthDays; i++) {
+      days.push(i);
+    }
+    this.setData({
+      days
+    });
+  },
+  handleCalendar(e) {
+    const handle = e.currentTarget.dataset.handle;
+    const cur_year = this.data.cur_year;
+    const cur_month = this.data.cur_month;
+    if (handle === 'prev') {
+      let newMonth = cur_month - 1;
+      let newYear = cur_year;
+      if (newMonth < 1) {
+        newYear = cur_year - 1;
+        newMonth = 12;
+      }
+
+      this.calculateDays(newYear, newMonth);
+      this.calculateEmptyGrids(newYear, newMonth);
+
+      this.setData({
+        cur_year: newYear,
+        cur_month: newMonth
+      })
+
+    } else {
+      let newMonth = cur_month + 1;
+      let newYear = cur_year;
+      if (newMonth > 12) {
+        newYear = cur_year + 1;
+        newMonth = 1;
+      }
+
+      this.calculateDays(newYear, newMonth);
+      this.calculateEmptyGrids(newYear, newMonth);
+
+      this.setData({
+        cur_year: newYear,
+        cur_month: newMonth
+      })
     }
   }
 })
